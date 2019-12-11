dd840a94f52936bc2ada296f0e5938040902ed26
real2rek <58668740+real2rek@users.noreply.github.com>
2019-12-11 20:03:42 +0100

    init

---
+++ SourceRLG/Models/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceRLG.Models
+{
+    public class Tile
+    {
+        public string Type { get; set; }
+
+        public string ModelPath { get; set; }
+
+        public int Rotation { get; set; }      
+    }
+}
